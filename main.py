9d578a62ab06d44140889839551d23392fd17a26
Dor Falah <dorfa@radware.com>
2024-06-16 16:18:31 +0300

    added debug mode

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def main():
     output = args.output
     verbose = args.verbose
     if verbose:
-        print("\nDebug mode")
+        print("\DEBUG mode")
     
     # configure scene and solver
     obstacles, robot, metadata = parseJson(sceneName=sceneName)
